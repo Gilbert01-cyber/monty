6accde473f1782a4a8d51179f951ae5b40825179
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 21:23:25 +0200

    Refactor stack operations with new function signatures

--- opcodes.c
+++ opcodes.c
@@ -1,83 +1,106 @@
 #include "monty.h"
 
-int is_number(char *str)
+/**
+ * op_push - pushes an element to the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: current line number in the file
+ */
+void op_push(stack_t **stack, unsigned int line_number)
 {
-    if (!str) return (0);
-    if (*str == '-') str++;
-    if (*str == '\0') return (0);
-    while (*str)
-    {
-        if (*str < '0' || *str > '9') return (0);
-        str++;
-    }
-    return (1);
+	stack_t *new_node;
+	char *arg;
+	int n, i;
+
+	arg = strtok(NULL, " \t\n\r");
+
+	if (!arg)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		free(g_line);
+		fclose(g_file);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Validate that arg is a valid integer */
+	i = 0;
+	if (arg[0] == '-')
+		i = 1;
+	for (; arg[i]; i++)
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			free(g_line);
+			fclose(g_file);
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	n = atoi(arg);
+
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(g_line);
+		fclose(g_file);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
 }
 
-void push(stack_t **stack, unsigned int line_number, char *arg)
+/**
+ * op_pall - prints all values on the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: current line number in the file
+ */
+void op_pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
-    if (!is_number(arg))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = atoi(arg);
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	stack_t *tmp;
+
+	(void)line_number;
+	tmp = *stack;
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
 
-void pall(stack_t **stack, unsigned int line_number)
+/**
+ * op_pop - removes the top element of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: current line number in the file
+ */
+void op_pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *curr = *stack;
-    (void)line_number;
-    while (curr)
-    {
-        printf("%d\n", curr->n);
-        curr = curr->next;
-    }
-}
+	stack_t *tmp;
 
-void pint(stack_t **stack, unsigned int line_number)
-{
-    if (!stack || !*stack)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
-}
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		free(g_line);
+		fclose(g_file);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
 
-void pop(stack_t **stack, unsigned int line_number)
-{
-    stack_t *tmp;
-    if (stack == NULL || *stack == NULL)
-    {
-        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    tmp = *stack;
-    *stack = (*stack)->next;
-    if (*stack != NULL)
-        (*stack)->prev = NULL;
-    free(tmp);
-}
+	tmp = *stack;
+	*stack = (*stack)->next;
 
-void free_stack(stack_t *stack)
-{
-    stack_t *tmp;
-    while (stack)
-    {
-        tmp = stack;
-        stack = stack->next;
-        free(tmp);
-    }
+	if (*stack)
+		(*stack)->prev = NULL;
+
+	free(tmp);
 }
