88fa4da0ac18c675f87ab05e29e904667a294b66
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 21:25:09 +0200

    Enhance monty.h with instruction struct and globals
    Added struct for instruction and global variables, updated stack structure.

--- monty.h
+++ monty.h
@@ -5,18 +5,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
-int is_number(char *str);
-void push(stack_t **stack, unsigned int line_number, char *arg);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t *stack);
-void pop(stack_t **stack, unsigned int line_number); 
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
-#endif
+/* Global variables */
+extern FILE *g_file;
+extern char *g_line;
+
+/* opcodes.c */
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+
+/* main.c */
+void free_stack(stack_t *stack);
+
+#endif /* MONTY_H */
