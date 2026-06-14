6fa2674c82e711b3029bb25a3f2f2f94dcf35eb4
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 21:34:47 +0200

    Add op_pint function prototype to monty.h

--- monty.h
+++ monty.h
@@ -42,6 +42,7 @@ void op_push(stack_t **stack, unsigned int line_number);
 void op_pall(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
 
 /* main.c */
 void free_stack(stack_t *stack);
