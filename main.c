8639b0c9d00c6949cb6ecf4f9980727c59e631ab
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 11:53:59 +0200

    Implement main loop and opcode dispatching
    Implement file reading and line processing for Monty interpreter.

--- main.c
+++ main.c
@@ -2,12 +2,45 @@
 
 int main(int argc, char *argv[])
 {
-    /* Check if user provided a file argument */
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+    char *opcode;
+
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
-    /* File opening and line processing logic will go here */
+
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (getline(&line, &len, file) != -1)
+    {
+        line_number++;
+        opcode = strtok(line, " \n\t");
+        if (!opcode)
+            continue;
+
+        if (strcmp(opcode, "push") == 0)
+        {
+            /* Call your push function here */
+            push(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack, line_number);
+        }
+    }
+
+    fclose(file);
+    free(line);
     return (0);
 }
