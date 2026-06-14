9ddc11999b3a0cdb2657298d406610a267bebbe3
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 11:48:45 +0200

    Add main entry point
    Implement main function to check for file argument

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+    /* Check if user provided a file argument */
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    /* File opening and line processing logic will go here */
+    return (0);
+}
