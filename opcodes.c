ccb92e16475eddd3b5f30ec3442dfe67ccb55caa
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 16:01:08 +0200

    Fix formatting and add newline at end of file

--- opcodes.c
+++ opcodes.c
@@ -9,7 +9,8 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n",
+		fprintf(stderr,
+			"L%u: can't pint, stack empty\n",
 			line_number);
 		exit(EXIT_FAILURE);
 	}
