8e0d6297fca33ce70ce2e7f615811448d27aa577
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 12:10:53 +0200

    Implement complete main loop and error handling

--- main.c
+++ main.c
@@ -7,40 +7,40 @@ int main(int argc, char *argv[])
     size_t len = 0;
     unsigned int line_number = 0;
     stack_t *stack = NULL;
-    char *opcode;
 
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
-
     file = fopen(argv[1], "r");
     if (!file)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-
     while (getline(&line, &len, file) != -1)
     {
         line_number++;
-        opcode = strtok(line, " \n\t");
-        if (!opcode)
+        char *opcode = strtok(line, " \n\t");
+        if (!opcode || opcode[0] == '#')
             continue;
-
         if (strcmp(opcode, "push") == 0)
         {
-            /* Call your push function here */
             push(&stack, line_number);
         }
         else if (strcmp(opcode, "pall") == 0)
         {
             pall(&stack, line_number);
         }
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
     }
-
-    fclose(file);
     free(line);
+    free_stack(stack);
+    fclose(file);
     return (0);
 }
