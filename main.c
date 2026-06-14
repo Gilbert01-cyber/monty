ae3c55fc35a9d67c23ee0232efda585f567681a3
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 14:13:48 +0200

    Fix indentation and formatting for opcode handling

--- main.c
+++ main.c
@@ -34,10 +34,13 @@ int main(int argc, char *argv[])
 		if (strcmp(opcode, "push") == 0)
 			push(&stack, line_number);
 		else if (strcmp(opcode, "pall") == 0)
-			else if (strcmp(opcode, "pall") == 0)
-		{
-			pall(&stack, line_number);
-		}
+        {
+            pall(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pint") == 0)
+        {
+            pint(&stack, line_number);
+        }
 		else if (strcmp(opcode, "pint") == 0)
 		{
 			pint(&stack, line_number);
