e955487490a25d0e6097e57d2235d8d265a28b21
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 16:25:39 +0200

    Define instruction_s struct for opcodes
    Added instruction_s struct for opcode handling.

--- monty.h
+++ monty.h
@@ -7,9 +7,6 @@
 
 /**
  * struct stack_s - doubly linked list representation of a stack
- * @n: integer
- * @prev: points to previous element
- * @next: points to next element
  */
 typedef struct stack_s
 {
@@ -18,7 +15,15 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-/* Function prototypes */
+/**
+ * struct instruction_s - opcode and its function
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
