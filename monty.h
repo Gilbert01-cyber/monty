528bd0948a0860e767ff8601508fea36c6762286
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 21:29:57 +0200

    Add op_swap function prototype to monty.h

--- monty.h
+++ monty.h
@@ -41,6 +41,7 @@ extern char *g_line;
 void op_push(stack_t **stack, unsigned int line_number);
 void op_pall(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
 
 /* main.c */
 void free_stack(stack_t *stack);
