117d96c71f97d40fda3194855b1341dcbac96516
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 20:20:48 +0200

    Clean up opcodes.c and improve code structure

--- opcodes.c
+++ opcodes.c
@@ -1,62 +1,3 @@
-#include "monty.h"
-
-int is_number(char *str)
-{
-    if (!str) return (0);
-    if (*str == '-') str++;
-    if (*str == '\0') return (0);
-    while (*str)
-    {
-        if (*str < '0' || *str > '9') return (0);
-        str++;
-    }
-    return (1);
-}
-
-void push(stack_t **stack, unsigned int line_number, char *arg)
-{
-    stack_t *new_node;
-
-    if (!is_number(arg))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = atoi(arg);
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
-}
-
-void pall(stack_t **stack, unsigned int line_number)
-{
-    stack_t *curr = *stack;
-    (void)line_number;
-    while (curr)
-    {
-        printf("%d\n", curr->n);
-        curr = curr->next;
-    }
-}
-
-void pint(stack_t **stack, unsigned int line_number)
-{
-    if (!stack || !*stack)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
-}
-
 void pop(stack_t **stack, unsigned int line_number)
 {
     stack_t *tmp;
@@ -71,15 +12,11 @@ void pop(stack_t **stack, unsigned int line_number)
     if (*stack != NULL)
         (*stack)->prev = NULL;
     free(tmp);
-}
+} /* Only one closing brace here */
+
+/* There should be no other } before the next function starts */
 
 void free_stack(stack_t *stack)
 {
-    stack_t *tmp;
-    while (stack)
-    {
-        tmp = stack;
-        stack = stack->next;
-        free(tmp);
-    }
+    /* ... */
 }
