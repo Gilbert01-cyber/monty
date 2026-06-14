ab61032f6641fef8626ad881dc2bf5707816c5fa
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 16:47:29 +0200

    Refactor stack_t and instruction_t structures

--- monty.h
+++ monty.h
@@ -5,29 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * struct stack_s - doubly linked list representation of a stack
- */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-void push(stack_t **stack, unsigned int line_number);
+int is_number(char *str);
+void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
-void push(stack_t **stack, unsigned int line_number, char *arg); 
 void free_stack(stack_t *stack);
 
 #endif
