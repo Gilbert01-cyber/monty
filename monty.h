a22c48816964d1aa309748621ea6a9271ea3b955
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 16:56:38 +0200

    Add pop function prototype to monty.h

--- monty.h
+++ monty.h
@@ -17,5 +17,7 @@ void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
+void pop(stack_t **stack, unsigned int line_number); 
+
 
 #endif
