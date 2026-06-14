6f4ed4d0bdf04cf3b88f97703ac3a86c00d787f3
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 14:06:08 +0200

    Implement pint function for stack top value
    Add pint function to print the top value of the stack

--- opcodes.c
+++ opcodes.c
@@ -59,5 +59,21 @@ void free_stack(stack_t *stack)
 		temp = stack;
 		stack = stack->next;
 		free(temp);
+		/**
+ * pint - prints the value at the top of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: current line number in the monty file
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
 	}
 }
