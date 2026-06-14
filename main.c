0c06aa01c40acdf82487244ff7cd14deb8d001f5
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 12:15:51 +0200

    Fix main.c logic to handle instructions and parsing

--- main.c
+++ main.c
@@ -1,46 +1,50 @@
 #include "monty.h"
 
+/**
+ * main - entry point for the Monty interpreter
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, EXIT_FAILURE on error
+ */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    unsigned int line_number = 0;
-    stack_t *stack = NULL;
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+	char *opcode;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    while (getline(&line, &len, file) != -1)
-    {
-        line_number++;
-        char *opcode = strtok(line, " \n\t");
-        if (!opcode || opcode[0] == '#')
-            continue;
-        if (strcmp(opcode, "push") == 0)
-        {
-            push(&stack, line_number);
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
-            pall(&stack, line_number);
-        }
-        else
-        {
-            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-    }
-    free(line);
-    free_stack(stack);
-    fclose(file);
-    return (0);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		opcode = strtok(line, " \n\t");
+		if (!opcode || opcode[0] == '#')
+			continue;
+		if (strcmp(opcode, "push") == 0)
+			push(&stack, line_number);
+		else if (strcmp(opcode, "pall") == 0)
+			pall(&stack, line_number);
+		else
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n",
+				line_number, opcode);
+			exit(EXIT_FAILURE);
+		}
+	}
+	free(line);
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
