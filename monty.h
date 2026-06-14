71654d23abdb00f880454703a7ddea9bae9ed57e
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 21:38:23 +0200

    Add op_add function prototype to monty.h

--- monty.h
+++ monty.h
@@ -43,6 +43,7 @@ void op_pall(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
 
 /* main.c */
 void free_stack(stack_t *stack);
