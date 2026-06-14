d1784c4e6eb9b579e50dad65c0f725026d92b212
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 22:45:10 +0200

    Implement op_pchar to print top stack character
    Add op_pchar function to print char at stack top

--- opcodes.c
+++ opcodes.c
@@ -294,3 +294,30 @@ void op_mod(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = NULL;
 	free(tmp);
 }
+
+/**
+ * op_pchar - prints the char at the top of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: current line number in the file
+ */
+void op_pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		fclose(g_file);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n",
+			line_number);
+		fclose(g_file);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", (*stack)->n);
+}
