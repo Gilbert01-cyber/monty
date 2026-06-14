c82c17bb5fb2f9b29527849f35e07533489b3e00
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 16:37:22 +0200

    Add overloaded push function with argument

--- monty.h
+++ monty.h
@@ -27,6 +27,7 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number, char *arg); 
 void free_stack(stack_t *stack);
 
 #endif
