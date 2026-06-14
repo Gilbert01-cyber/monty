1383d110517302ced4c0d296097f877d702e2334
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 12:20:33 +0200

    Refactor push and implement pall functions
    Refactored push function to handle stack operations and added pall function to print stack values.

--- opcodes.c
+++ opcodes.c
@@ -1,33 +1,63 @@
+#include "monty.h"
+
 /**
- * is_digit - checks if a string is a number
- * @s: string to check
- * Return: 1 if number, 0 if not
+ * push - pushes an element to the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: current line number
  */
-int is_digit(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	if (s[0] == '-')
-		i++;
-	for (; s[i]; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-	}
-	return (1);
-}
-
 void push(stack_t **stack, unsigned int line_number)
 {
 	char *arg = strtok(NULL, " \n\t");
 	stack_t *new_node;
 
-	if (!is_digit(arg))
+	if (!arg || (atoi(arg) == 0 && strcmp(arg, "0") != 0))
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	/* ... rest of your malloc and link logic ... */
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = atoi(arg);
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
+
+/**
+ * pall - prints all values on the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: current line number
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	(void)line_number;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+/**
+ * free_stack - frees a stack_t linked list
+ * @stack: pointer to the head of the stack
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *temp;
+
+	while (stack)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
 }
