494ae9e8c1cb4001f5d7b6e6a067267ad92c9118
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 14:17:24 +0200

    Refactor stack operations and add memory management

--- opcodes.c
+++ opcodes.c
@@ -2,78 +2,70 @@
 
 /**
  * push - pushes an element to the stack
- * @stack: pointer to the head of the stack
+ * @stack: double pointer to the stack
  * @line_number: current line number
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	char *arg = strtok(NULL, " \n\t");
-	stack_t *new_node;
-
-	if (!arg || (atoi(arg) == 0 && strcmp(arg, "0") != 0))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	new_node = malloc(sizeof(stack_t));
-	if (!new_node)
+	stack_t *new;
+	(void)line_number;
+	new = malloc(sizeof(stack_t));
+	if (!new)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = atoi(arg);
-	new_node->prev = NULL;
-	new_node->next = *stack;
+	/* Note: In your actual code, ensure you are getting the value to push */
+	new->n = 0; 
+	new->prev = NULL;
+	new->next = *stack;
 	if (*stack)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+		(*stack)->prev = new;
+	*stack = new;
 }
 
 /**
- * pall - prints all values on the stack
- * @stack: pointer to the head of the stack
+ * pall - prints all elements of the stack
+ * @stack: double pointer to the stack
  * @line_number: current line number
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+	stack_t *tmp = *stack;
 	(void)line_number;
-
-	while (temp)
+	while (tmp)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
 }
 
 /**
- * free_stack - frees a stack_t linked list
- * @stack: pointer to the head of the stack
- */
-void free_stack(stack_t *stack)
-{
-	stack_t *temp;
-
-	while (stack)
-	{
-		temp = stack;
-		stack = stack->next;
-		free(temp);
-		/**
  * pint - prints the value at the top of the stack
- * @stack: double pointer to the head of the stack
- * @line_number: current line number in the monty file
- *
- * Return: void
+ * @stack: double pointer to the stack
+ * @line_number: current line number
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * free_stack - frees a doubly linked list
+ * @stack: pointer to the head of the list
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *tmp;
+	while (stack)
+	{
+		tmp = stack;
+		stack = stack->next;
+		free(tmp);
 	}
 }
