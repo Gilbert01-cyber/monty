0d108acf50d639e6eb0b1d2ab1571b841b06a681
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-14 14:20:41 +0200

    Validate integer input for push operation

--- opcodes.c
+++ opcodes.c
@@ -1,22 +1,45 @@
 #include "monty.h"
 
 /**
+ * is_number - checks if a string is a valid integer
+ * @s: string to check
+ * Return: 1 if number, 0 otherwise
+ */
+int is_number(char *s)
+{
+	int i = 0;
+
+	if (!s) return (0);
+	if (s[0] == '-') i++;
+	for (; s[i]; i++)
+	{
+		if (s[i] < '0' || s[i] > '9') return (0);
+	}
+	return (1);
+}
+
+/**
  * push - pushes an element to the stack
- * @stack: double pointer to the stack
+ * @stack: double pointer to the head of the stack
  * @line_number: current line number
  */
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
-	(void)line_number;
+	char *arg = strtok(NULL, " \n\t");
+
+	if (!is_number(arg))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	new = malloc(sizeof(stack_t));
 	if (!new)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	/* Note: In your actual code, ensure you are getting the value to push */
-	new->n = 0; 
+	new->n = atoi(arg);
 	new->prev = NULL;
 	new->next = *stack;
 	if (*stack)
@@ -25,24 +48,8 @@ void push(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * pall - prints all elements of the stack
- * @stack: double pointer to the stack
- * @line_number: current line number
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp = *stack;
-	(void)line_number;
-	while (tmp)
-	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-	}
-}
-
-/**
  * pint - prints the value at the top of the stack
- * @stack: double pointer to the stack
+ * @stack: double pointer to the head of the stack
  * @line_number: current line number
  */
 void pint(stack_t **stack, unsigned int line_number)
